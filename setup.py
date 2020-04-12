40d3da21b8096391ce017fe9dd0fda84a98776b9
Kohei YOSHIDA <kohei@yosida95.com>
2020-04-12 19:13:25 +0900

    drop support for Python < 3.6

--- setup.py
+++ setup.py
@@ -8,40 +8,37 @@ from setuptools import (
 )
 
 here = os.path.dirname(__file__)
-requires = []
-tests_require = []
-
-try:
-    import enum
-except ImportError:
-    requires.append('enum34')
-else:
-    del enum
 
 
 def _read(name):
     try:
         return open(os.path.join(here, name)).read()
-    except:
+    except BaseException:
         return ""
+
+
 readme = _read("README.rst")
 license = _read("LICENSE")
 
 setup(
     name='sshkey',
     version='0.0.1',
-    test_suite='sshkey',
-    author='Kohei YOSHIDA',
-    author_email='license@yosida95.com',
     description='SSH key management utility',
     long_description=readme,
     license=license,
     url='https://github.com/yosida95/python-sshkey',
+
+    author='Kohei YOSHIDA',
+    author_email='license@yosida95.com',
+
     packages=find_packages(),
-    install_requires=requires,
-    tests_require=tests_require,
+    test_suite='tests',
+    install_requires=[],
+    tests_require=[],
+    python_requires='>= 3.6',
+
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
